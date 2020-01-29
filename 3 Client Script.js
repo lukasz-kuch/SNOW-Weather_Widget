bb14fd1abf1e44b519af9af458081b0c19288f5b
Kucharski <lukasz.kucharski@fujitsu.com>
2020-01-29 12:26:27 +0100

    added comments

--- 3 Client Script.js
+++ 3 Client Script.js
@@ -1,7 +1,7 @@
 function() {
     /* widget controller */
     var c = this;
-
+    //after buttin clicked, client controller send info to serv3er side
     c.getDetails = function() {
         c.values = true;
         c.data.action = "getCity";
